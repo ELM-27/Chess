76e84b0ecfce447c8828f3145a026f51067f9080
ELM-27 <elliott.m.lowman@gmail.com>
2025-02-02 22:02:22 -0500

    Initial commit

---
+++ src/Board.java
@@ -0,0 +1,67 @@
+import java.util.*;
+import java.io.*;
+
+public class Board {
+    private Piece[][] board;
+
+    // Pieces are defined individually for easy access
+    /*
+     * Location of pieces in index (from left to right when multiple of the same piece are present)
+     * Index 4:       King
+     * Index 3:       Queen
+     * Index 0, 7:    Rooks
+     * Index 2, 5:    Bishops
+     * Index 1, 6:    Knights
+     * Index 8 - 15:  Pawns
+     */
+    private Piece[] white = new Piece[8];
+    private Piece[] whitePawn = new Piece[8];
+    private Piece[] black = new Piece[8];
+    private Piece[] blackPawn = new Piece[8];
+
+    public Board() {
+        board = buildBoard();
+    }
+
+    public void printBoard() {
+        for(int i = 0; i < 8; i++) {
+            for(int j = 0; j < 8; j++) {
+                System.out.print(board[i][j].getPiece() + "" + board[i][j].getColor() + " ");
+            }
+
+            System.out.println();
+        }
+    }
+
+    private Piece[][] buildBoard() {
+        Piece[][] newBoard = new Piece[8][8];
+
+        for(int i = 0; i < 8; i++) {
+            white[i] = new Piece(7, i, i, false);
+            newBoard[7][i] = white[i];
+        }
+
+        for(int i = 0; i < 8; i++) {
+            whitePawn[i] = new Piece(6, i, 8, false);
+            newBoard[6][i] = whitePawn[i];
+        }
+
+        for(int i = 0; i < 8; i++) {
+            for(int j = 2; j < 6; j++) {
+                newBoard[j][i] = new Piece(j, i, -1, false);
+            }
+        }
+
+        for(int i = 0; i < 8; i++) {
+            blackPawn[i] = new Piece(1, i, 8, true);
+            newBoard[1][i] = blackPawn[i];
+        }
+
+        for(int i = 0; i < 8; i++) {
+            black[i] = new Piece(0, i, i, true);
+            newBoard[0][i] = black[i];
+        }
+
+        return newBoard;
+    }
+}
---
+++ src/Piece.java
@@ -0,0 +1,51 @@
+public class Piece {
+
+    private int locX;
+    private int locY;
+
+    /*
+     * Type of piece indicated by the following values:
+     * 4:       King
+     * 3:       Queen
+     * 0, 7:    Rooks
+     * 2, 5:    Bishops
+     * 1, 6:    Knights
+     * 8 - 15:  Pawns
+     */
+    private int type;
+
+    // black pieces are true, white pieces are false
+    private boolean color;
+
+    public Piece(int locX, int locY, int type, boolean color) {
+        this.locX = locX;
+        this.locY = locY;
+        this.type = type;
+        this.color = color;
+    }
+
+    public char getColor() {
+        if(color) {
+            return '-';
+        } else {
+            return ' ';
+        }
+    }
+    public char getPiece() {
+        if (type == 4) {
+            return 'K';
+        } else if (type == 3) {
+            return 'Q';
+        } else if (type == 0 || type == 7) {
+            return 'R';
+        } else if (type == 2 || type == 5) {
+            return 'B';
+        } else if (type == 1 || type == 6) {
+            return 'N';
+        } else if (type == 8) {
+            return 'P';
+        } else {
+            return ' ';
+        }
+    }
+}
